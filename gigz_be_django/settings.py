c07232ef06cde2f8177e1acd8eb642f74420a6d7
2magpies <daphnemintz@yahoo.com>
2020-03-12 18:24:34 -0400

    added heroku stuff to settings

--- gigz_be_django/settings.py
+++ gigz_be_django/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
+import dj_database_url
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'gigz-be.herokuapp.com']
 
 
 # Application definition
@@ -107,6 +110,9 @@ DATABASES = {
  	}
  }
 
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
+
 
 
 # Password validation
@@ -146,3 +152,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
